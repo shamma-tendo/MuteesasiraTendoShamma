7294e3d057d689ac099529e07fa9286a6ac75537
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-06-18 20:05:20 +0300

    ttt

--- tuples/phone.py
+++ tuples/phone.py
@@ -8,3 +8,9 @@ phonelist = list(x)
 phonelist[1] = "itel" 
 x = tuple(phonelist)
 print(x)
+#addint item to tuple
+x = x + ("huawei",)
+print(x)
+#delete 1st item from tuple
+x = x[:1] + x[2:5]
+print(x)
