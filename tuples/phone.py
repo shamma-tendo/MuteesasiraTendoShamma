f796d313c292fe39b98c4d9cae896aa4f0aa60bf
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-06-18 20:00:44 +0300

    ttt

--- tuples/phone.py
+++ tuples/phone.py
@@ -3,3 +3,8 @@ x = ("samsung", "iphone", "tecno", "redmi")
 print(x[-1])
 #print 2nd last item from the tuple
 print(x[-2])
+#update an item in a tuple
+phonelist = list(x)
+phonelist[1] = "itel" 
+x = tuple(phonelist)
+print(x)
