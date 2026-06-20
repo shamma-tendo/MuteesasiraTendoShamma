264f2d2e70ba35e9e6b359b87d5cd992934aaa07
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-06-20 16:38:41 +0300

    spaces

---
+++ strings/spaces.py
@@ -0,0 +1,15 @@
+txt = "Hello, Uganda! "
+
+# Remove spaces at the beginning and end using strip()
+print(txt.strip())
+
+# To remove spaces in the middle, you'd need to specify which spaces
+# For example, removing all spaces:
+print(txt.replace(" ", ""))
+
+# Or removing specific spaces (like between Hello, and Uganda):
+print(txt.replace(" ", ""))
+
+#convert value of txt to upper case
+txt = "Hello, Uganda! "
+print(txt.upper())
