c9d5f60b0b9ecf305e75db02ead63eec7372388f
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-06-22 09:34:39 +0300

    ddd

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,20 @@
+shoes={
+    "brand" : "nick",
+    "color" : "black",
+    "size" : 40
+}
+
+#print value of shoe size
+print(shoes["size"])
+#change value of nick to adidas
+shoes["brand"] = "adidas"
+print(shoes)
+#add value type"sneakers"
+shoes["type"] = "sneakers"
+print(shoes)
+#return list of all keys in the dictionary
+list(shoes.keys())
+print(shoes)
+#return a list of all values in the dictionary
+list(shoes.values())
+print(shoes)
